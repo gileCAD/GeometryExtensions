109e8f29569338fc2ec60749afff78ade40d4839
Gilles Chanteau <gilecad@orange.fr>
2026-02-18 16:18:00 +0100

    feat: add BrepFaceExtension class

--- GeometryExtensionsR20/BoundaryLoopExtension.cs
+++ GeometryExtensionsR20/BoundaryLoopExtension.cs
@@ -1,7 +1,9 @@
 ﻿using Autodesk.AutoCAD.BoundaryRepresentation;
+using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Gile.AutoCAD.R20.Geometry
 {
@@ -24,6 +26,38 @@ namespace Gile.AutoCAD.R20.Geometry
                 using (var externalCurve = (ExternalCurve3d)edge.Curve)
                 {
                     yield return externalCurve.NativeCurve;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the curves constituting the boundary loop.
+        /// </summary>
+        /// <param name="loop">The instance to which this method applies.</param>
+        /// <param name="tolerance">Tolerance used in curve end points comparison.</param>
+        /// <returns>Curve collection.</returns>
+        /// <exception cref="System.ArgumentNullException">ArgumentException is thrown if <paramref name="loop"/> is null.</exception>
+        public static IEnumerable<Curve> GetCurves(this BoundaryLoop loop, Tolerance tolerance = default)
+        {
+            Assert.IsNotNull(loop, nameof(loop));
+
+            if (tolerance.Equals(default(Tolerance)))
+                tolerance = Tolerance.Global;
+
+            var curves3d = loop.GetNativeCurves().ToArray();
+            if (curves3d.Length == 1)
+            {
+                yield return Curve.CreateFromGeCurve(curves3d[0]);
+            }
+            else if (curves3d.TryConvertToCompositeCurve(out CompositeCurve3d compositeCurve, tolerance, c => c is LineSegment3d || c is CircularArc3d))
+            {
+                yield return (Polyline)Curve.CreateFromGeCurve(compositeCurve);
+            }
+            else
+            {
+                foreach (Curve3d curve3d in curves3d)
+                {
+                    yield return Curve.CreateFromGeCurve(curve3d);
                 }
             }
         }
